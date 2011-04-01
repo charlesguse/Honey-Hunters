ff82956c9a5ed87baf4367a3ce78fcc16cb8fb1d
Blake Robertson <blaquer@gmail.com>
2011-03-31 23:12:43 -0400

    Canvas shows proper images and detects clicks

--- frontend/webpage/HoneyHuntersGame.js
+++ frontend/webpage/HoneyHuntersGame.js
@@ -5,8 +5,10 @@
 
 function HoneyHuntersGame () {
   this.oRes = new ResourceConfig();
-  this.oBoard = new Array();
+  this.aBoard = new Array();
+  this.aBoardCenters = new Array();
   
+  this.nScaleFactor = 1.0;
   this.imgCellEmpty = new Image();
   this.imgCellHoney = new Image();
   this.imgCellHidden = new Image();
@@ -26,6 +28,7 @@ HoneyHuntersGame.prototype.initGame = function() {
   
   while (this.nImagesToLoad < this.nImagesLoaded) { }
   
+  this.wireEvents();
   this.runGame();
 }
 
@@ -52,27 +55,95 @@ HoneyHuntersGame.prototype.loadEnvironmentData = function() {
 }
 
 HoneyHuntersGame.prototype.loadGameData = function() {
-  // build board
-  // set player data
+  this.aBoard = [[-2,3,-1],[2,-1,-2],[-1,-2,3],[-2,4,-1]];
+  this.aBoardCenters = [[],[],[],[]];
+  
+  var cellWidth = this.imgCellEmpty.width * this.nScaleFactor;
+  var cellHeight = this.imgCellEmpty.height * this.nScaleFactor;
+  var cellCenterX = 0;
+  var cellCenterY = 0;
+  for (var i=0; i<this.aBoard.length; i++) {
+    for (var j=0; j<this.aBoard[i].length; j++) {
+      cellCenterX = cellWidth * (i * 0.75 + 0.5);
+      cellCenterY = cellHeight * (j + 0.5);
+      if (!(i % 2)) {
+        cellCenterY += 0.5 * cellHeight;
+      }
+      
+      this.aBoardCenters[i][j] = {"x":cellCenterX, "y":cellCenterY}; 
+    }
+  }
 }
 
-HoneyHuntersGame.prototype.runGame = function() {
-  //this.ctx.clearRect(0,0,this.screenWidth,this.screenHeight);
-  //this.ctx.save();
-  var scaleFactor = 0.5;
-  var cellWidth = this.imgCellEmpty.width * scaleFactor;
-  var cellHeight = this.imgCellEmpty.height * scaleFactor;
+lineLength = function(x, y, x0, y0){
+    return Math.sqrt((x -= x0) * x + (y -= y0) * y);
+};
+
+HoneyHuntersGame.prototype.findNearestCenter = function(x, y) {
+  var best;
+  var minDistance = Number.MAX_VALUE;
+  var dist;
+  for (var i=0; i<this.aBoard.length; i++) {
+    for (var j=0; j<this.aBoard[i].length; j++) {
+      dist = lineLength(x, y, this.aBoardCenters[i][j].x, this.aBoardCenters[i][j].y);
+      if (dist < minDistance) {
+        minDistance = dist;
+        best = {"i":i, "j":j};
+      }
+    }
+  }
+  
+  return best;
+}
+
+HoneyHuntersGame.prototype.onClick = function(e) {
+  var hex = e.data.findNearestCenter(e.offsetX,e.offsetY);
+  var clickX = e.data.aBoardCenters[hex.i][hex.j].x;
+  var clickY = e.data.aBoardCenters[hex.i][hex.j].y;
+  e.data.ctx.fillRect(clickX-10, clickY-10, 20, 20);
+}
+
+HoneyHuntersGame.prototype.wireEvents = function() {
+  $("#honeyHuntersCanvas").bind('click', this, this.onClick);
+}
+
+HoneyHuntersGame.prototype.draw = function() {
+  var cellWidth = this.imgCellEmpty.width * this.nScaleFactor;
+  var cellHeight = this.imgCellEmpty.height * this.nScaleFactor;
+  var cellText = "";
+  var fontSize = (cellHeight * 0.5);
   var cellX = 0;
   var cellY = 0;
+  var cellImage;
   
-  for (var i=0; i<9; i++) {
-    for (var j=0; j<6; j++) {
+  this.ctx.clearRect(0,0,this.screenWidth,this.screenHeight);
+  //this.ctx.save();
+  this.ctx.font = fontSize + "px Comic Sans";
+  for (var i=0; i<this.aBoard.length; i++) {
+    for (var j=0; j<this.aBoard[i].length; j++) {
       cellX = i * cellWidth * 0.75 ;
       cellY = j * cellHeight;
       if (!(i % 2)) {
         cellY += 0.5 * cellHeight;
       }
-      this.ctx.drawImage(this.imgCellEmpty, cellX, cellY, cellWidth, cellHeight);
+      
+      switch(this.aBoard[i][j]) {
+        case -2: cellImage = this.imgCellHidden;
+          cellText = "";
+          break;
+        case -1: cellImage = this.imgCellHoney;
+          cellText = "";
+          break;
+        default: cellImage = this.imgCellEmpty;
+          cellText = this.aBoard[i][j];
+          break;
+      }
+      this.ctx.drawImage(cellImage, cellX, cellY, cellWidth, cellHeight);
+      this.ctx.fillText(cellText, this.aBoardCenters[i][j].x - (fontSize*0.25), this.aBoardCenters[i][j].y + (fontSize*0.25))
     }
   }
+}
+
+HoneyHuntersGame.prototype.runGame = function() {
+  this.draw();
 }
