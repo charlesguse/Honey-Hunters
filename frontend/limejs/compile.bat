95e8f45d9951eee7467af7e47e135b5ca37046cd
Charlie Guse <charlesguse@users.noreply.github.com>
2012-03-04 16:01:23 -0500

    Removed extra requires goog.requires that weren't necessary and also added the advanced compile option to the compile.bat

--- frontend/limejs/compile.bat
+++ frontend/limejs/compile.bat
@@ -2,5 +2,5 @@
 SETLOCAL
 set startingDir=%CD%
 cd \sandbox\lime
-python bin/lime.py build honeyhunters -o ..\honeyhunters\frontend\limejs\honeyhunters\compiled\honeyhunters.js
+python bin/lime.py build honeyhunters -o ..\honeyhunters\frontend\limejs\honeyhunters\compiled\honeyhunters.js -a
 cd %startingDir%
