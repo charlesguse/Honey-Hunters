46fbb5e656e50df92211ad3a3ba19a3c6af79c61
Charlie Guse <charlesguse@users.noreply.github.com>
2012-04-20 05:31:54 -0400

    Renamed the files to better follow PEP 8. Must... be... a... sheeple to PEP 8... Disregard any other PEP 8 issues I may have in these files for now... I haven't read all of it yet :)

--- backend/Honey Hunters/tests/game_unit_test_suite.py
+++ backend/Honey Hunters/tests/game_unit_test_suite.py
@@ -1,10 +1,10 @@
 import unittest
-import gameBoardUnittests
-import gameManagementUnittests
+import game_board_unittests
+import game_management_unittests
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(gameBoardUnittests.TestGameBoardHex))
-    suite.addTest(unittest.makeSuite(gameManagementUnittests.TestGameManagement))
+    suite.addTest(unittest.makeSuite(game_board_unittests.TestGameBoardHex))
+    suite.addTest(unittest.makeSuite(game_management_unittests.TestGameManagement))
     
     unittest.TextTestRunner(verbosity=1).run(suite)
